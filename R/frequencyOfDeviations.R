5149c3071e4644bd03e707745678386becd37876
acnb <code@bietenbeck.net>
2017-08-01 15:56:27 +0200

    fix faceted histogramm

--- R/frequencyOfDeviations.R
+++ R/frequencyOfDeviations.R
@@ -6,7 +6,7 @@ quant <- eqaAll %>%
             p975 = quantile(relDiff, .975, names = F)) %>%
   gather(value=x, key=q, p025, p975)
 
-ymax = .05
+ymax = .15
 
 poly <- data.frame(x = c(quant$x, quant$x[nrow(quant):1]), 
                    eqa = levels(eqaAll$eqa)[
@@ -16,11 +16,19 @@ poly <- data.frame(x = c(quant$x, quant$x[nrow(quant):1]),
 
 eqaData <- eqaAll %>%
   filter(!is.na(relDiff)) %>%
-  filter(abs(relDiff) < .5)
+  filter(abs(relDiff) < .5) %>%
+  group_by(eqa) %>%
+  mutate(class = cut(relDiff, breaks=seq(-.3, .3, .01), 
+                     labels = seq(-.3+.01, .3, .01)-(.01/2))
+         , n= n()) %>%
+  group_by(eqa, class) %>%
+  summarise(p = n()/n[1]) %>%
+  ungroup() %>%
+  mutate(class = as.numeric(as.character(class)))
+  
 
 ggplot() +
-  geom_histogram(data = eqaData, aes(x=relDiff, y = (..count..)/sum(..count..)), 
-                 binwidth = .01) +
+  geom_col(data = eqaData, aes(x=class, y = p))+
   scale_x_continuous(limits = c(-.3,.3), labels=percent) +
   geom_polygon(data = poly, aes(x=x, y=y), 
                fill = "red", alpha=.1) +
