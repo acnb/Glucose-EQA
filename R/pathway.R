48a1ca5dc6400fb0f219bbb60bc88598c11846aa
acnb <code@bietenbeck.net>
2017-11-02 14:47:57 +0100

    labels for pathway figures

--- R/pathway.R
+++ R/pathway.R
@@ -59,7 +59,7 @@ outcomeStats <- devChanges %>%
   mutate(eqa = as.character(eqa)) %>%
   mutate(eqa = if_else(str_length(eqa) > 7,
                        str_replace(eqa, '-', "-\n"),
-                       eqa))
+                       eqa)) 
 
 outcomeFailedAgain <- devChanges %>%
   filter(pHasFailed) %>%
@@ -75,13 +75,14 @@ outcomeFailedAgain <- devChanges %>%
   mutate(eqa = as.character(eqa)) %>%
   mutate(eqa = if_else(str_length(eqa) > 7, 
          str_replace(eqa, '-', "-\n"), 
-         eqa))
+         eqa)) 
 
 
 ggplot(outcomeStats, aes(x=act, y=p, label = n, fill =  pHasFailed)) +
   geom_rect(data = outcomeStats, aes(fill = type),
             xmin = -Inf,xmax = Inf, ymin = -Inf,ymax = Inf) +
   geom_col(position = 'dodge')+
+  geom_text(position = position_dodge(width = 1), aes(y=p+0.05),  size=2.5)+
   scale_y_continuous(labels=percent, limits = c(0, 1))+
   scale_x_discrete(labels = c(
     'left' = 'left EQA',
@@ -94,8 +95,8 @@ ggplot(outcomeStats, aes(x=act, y=p, label = n, fill =  pHasFailed)) +
                'FALSE' = 'after year without failures'),
     breaks = c('TRUE', 'FALSE')) +
   facet_grid(.~eqa)+ 
-  xlab("yearly actions in EQAs") +
-  ylab('percentage of participants in each year') +  
+  xlab("pathway of EQA participants") +
+  ylab('participants') +  
   theme_pub(base_size = 10) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1), 
         legend.title = element_blank())
@@ -123,20 +124,23 @@ changeGraphN <- eqaAll %>%
   filter(pHasFailed) %>%
   group_by(eqa, devNotFailedBefore) %>%
   summarise(n=n_distinct(id)) %>%
-  ungroup()
+  group_by(eqa) %>%
+  mutate(p=n/sum(n)) %>%
+  ungroup() %>%
+  mutate(label = paste0(n, "\n[", round(p,2)*100, "%]", sep=""))
 
 ggplot() + 
   geom_col(data = changeGraph, aes(x=devNotFailedBefore,  
                                    y=p, fill=status),
            position = position_stack(reverse = TRUE)) +
   geom_text(data = changeGraphN, aes(x=devNotFailedBefore, 
-                                     y=1.1, label=n),size=3)+ 
+                                     y=1.1, label=label),size=3)+ 
   scale_y_continuous(labels=percent, breaks = c(0,.25, .5, .75, 1)) +
   scale_x_discrete(labels = c('TRUE' = 'new device', 
                               'FALSE' = 'no new device')) + 
   scale_fill_manual(values=colors.status) +
   xlab('performance after year\n with failed EQA') +
-  ylab('percentage of EQA participations') +
+  ylab('individual EQA participations') +
   theme_pub() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1), 
         legend.title = element_blank()) +
